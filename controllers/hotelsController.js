abda7bc4d50d1b69d06eef76a82dc485887de3c1
fikri <me@fikri.tech>
2020-10-28 09:08:10 +0700

    new model

--- controllers/hotelsController.js
+++ controllers/hotelsController.js
@@ -2,7 +2,7 @@ const db = require('./../database/mysql')
 module.exports = {
     getAllHotels : (req,res) => {
 
-        db.query("SELECT h.id,h.` name`, min(r.price) as price,address, phone,star FROM hotels h JOIN rooms r on h.id = r.hotels_id GROUP BY h.` name`;",(err,result) => {
+        db.query("SELECT h.id,h.` name`, min(r.price) as price,address, phone,star,hi.url FROM hotels h JOIN rooms r on h.id = r.hotels_id JOIN hotel_images hi on hi.hotels_id = h.id GROUP BY h.` name`;",(err,result) => {
             try {
                 if(err) throw err
                 res.send({
