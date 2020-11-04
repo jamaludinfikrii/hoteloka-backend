119cfc64afb942ceaf9844173e3ec3df869ef67b
fikri <me@fikri.tech>
2020-11-04 12:09:47 +0700

    room left

--- controllers/hotelsController.js
+++ controllers/hotelsController.js
@@ -32,7 +32,8 @@ module.exports = {
 
     getHotelDetailById : async (req,res) => {
         const id = req.params.id
-        let queryRooms = 'SELECT r.id,r.`name`, guest_count,price,hotels_id,room_counts, GROUP_CONCAT(DISTINCT(ri.url)) as room_image FROM rooms r JOIN room_images ri on r.id = ri.rooms_id WHERE hotels_id = ? GROUP BY rooms_id; '
+        const date = req.query.date
+        let queryRooms = `SELECT r.id,r.name, guest_count,price,hotels_id,room_counts, GROUP_CONCAT(DISTINCT(ri.url)) as room_image,get_count_rooms_available(?,r.id,room_counts) as room_left FROM rooms r JOIN room_images ri on r.id = ri.rooms_id WHERE hotels_id = ? GROUP BY rooms_id;`
         let queryFacilities = 'SELECT facilities_id,`name`,icon FROM hotels_has_facilities h JOIN facilities f ON h.facilities_id = f.id WHERE h.hotels_id = ?;'
         let queryHotelImages = 'SELECT * FROM hotel_images WHERE hotels_id = ?;'
         let queryHotelsData = 'select * from hotels where id = ?'
@@ -56,9 +57,10 @@ module.exports = {
         // })
 
         try {
+            console.log(date)
             let hotelsData = await query(queryHotelsData,id)
             let facilities = await query(queryFacilities,id)
-            let rooms = await query(queryRooms,id)
+            let rooms = await query(queryRooms,[date,id])
             let hotelImages = await query(queryHotelImages,id)            
 
             res.send({
