8195d92c8cdac529642f7469579132895060bb7f
fikri <me@fikri.tech>
2020-11-12 14:39:47 +0700

    redis

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,5 +1,6 @@
 const db = require('./../database/mysql')
 const sendNotif = require('./../helpers/sendNotif')
+const redis = require('./../database/redis')
 
 module.exports = {
     createTransaction : (req,res) => {
@@ -60,7 +61,8 @@ module.exports = {
                             app_id: "4d8e89c1-14ee-497e-a2a0-f6526cf4b285",
                             contents: {"en": "Transaction Approved"},
                             channel_for_external_user_ids: "push",
-                            include_external_user_ids: [data.token]
+                            include_external_user_ids: [data.token],
+                            url : "hotelokaApp://hommy/detailHotel/2"
                         }
                         sendNotif(dataNotif,res)
 
@@ -75,5 +77,64 @@ module.exports = {
         // update transaction status
         // kill event auto cancel
         // send notification one signal
+    }
+
+    ,
+    getTransactionByIdUser : (req,res) => {
+        var start = Date.now()
+
+        redis.get('all_transactions',(err,redisData) => {
+            if(err) throw err
+            if(redisData){
+                var end = Date.now()
+                var resTime = end - start
+                var redisParsed = JSON.parse(redisData)
+                res.send({
+                    resTime,
+                    redisParsed
+                })
+            }else{
+                db.query('select * from transactions', (err,result) => {
+                    if(err) throw err
+                    var end = Date.now()
+                    var resultString = JSON.stringify(result)
+                    redis.set('all_transactions',resultString,(err,ok) => {
+                        if(err) throw err
+                        var resTime = end - start
+                        res.send({
+                            resTime,
+                            result
+                        })
+                    })
+    
+    
+                    
+                })
+            }
+        })
+        // let redisData = redis.get('all_transactions')
+        // if(redisData.length > 0){
+        //     var end = Date.now()
+        //     var resTime = end - start
+        //     res.send({
+        //         resTime,
+        //         redisData
+        //     })
+        // }else{
+        //     db.query('select * from transactions', (err,result) => {
+        //         if(err) throw err
+        //         var end = Date.now()
+                
+        //         redis.set('all_transactions',result)
+
+
+        //         var resTime = end - start
+        //         res.send({
+        //             resTime,
+        //             result
+        //         })
+        //     })
+        // }
+
     }
 }
--- routers/transactionRouter.js
+++ routers/transactionRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express').Router()
 const jwtVerify = require('../middleware/jwt')
-const {createTransaction, paymentApproved} = require('./../controllers/transactionController')
+const {createTransaction, paymentApproved, getTransactionByIdUser} = require('./../controllers/transactionController')
 
 Router.post('/',createTransaction)
 Router.post('/payment-approve',jwtVerify,paymentApproved)
+Router.get('/transactionusers/:id',getTransactionByIdUser)
 
 module.exports = Router
 
